14314c595c40847cc4844549a333e351a6d28d54
DouglasSAraujo <douglas.as.016c@hotmail.com>
2020-12-26 22:11:44 -0300

    Start Project RPG Game

---
+++ Game_RPG/src/game_rpg/Main.java
@@ -0,0 +1,16 @@
+package game_rpg;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println("\nBEM VINDO AO TRIFORCE\n");
+        
+        System.out.println("\nFaça a sua escolha:");
+        
+        System.out.println("\n1 - Criar Novo Personagem");
+        System.out.println("2 - Carregar Personagem");
+        System.out.println("3 - Sair do Game\n");
+    }
+    
+}
