b75602dacd96e1bac415b5868b2340fb883081f7
CarlosEstellita <74671164+CarlosEstellita@users.noreply.github.com>
2021-01-22 21:34:23 -0300

    Update Mago.java

---
+++ Game_RPG/src/game_rpg/Mago.java
@@ -0,0 +1,13 @@
+import java.util.*;
+//poder especial-Regenera 25 de vida
+class Mago extends Personagem{
+  
+  public Mago(String nome, int tribo, int classe){
+    super(nome, tribo, classe);
+  }
+  
+  public void poderEspecial(int jogador, Vector<Personagem> personagem_partida){
+     personagem_partida.get(jogador).setVida(personagem_partida.get(jogador).getVida()+25);
+  }
+
+}
