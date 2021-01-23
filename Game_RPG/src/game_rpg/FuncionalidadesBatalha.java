d4acfb00db9700a075605879dd32cdccb1bdb49b
CarlosEstellita <74671164+CarlosEstellita@users.noreply.github.com>
2021-01-22 21:33:21 -0300

    Update FuncionalidadesBatalha.java

---
+++ Game_RPG/src/game_rpg/FuncionalidadesBatalha.java
@@ -0,0 +1,10 @@
+public interface FuncionalidadesBatalha
+{
+  public void atacar(int atacante, int oponente);
+  public void defender(int jogador);
+  public void powerUp(int jogador);
+  public void mostrarPersonagens();
+  public int personagemVivos();
+  public void luta();
+  public void criarPersonagem();
+}
