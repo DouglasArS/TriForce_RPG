af55860c74ed39f02f0578f72978e63ab55a5222
CarlosEstellita <74671164+CarlosEstellita@users.noreply.github.com>
2021-01-22 21:33:57 -0300

    Update Guerreiro.java

---
+++ Game_RPG/src/game_rpg/Guerreiro.java
@@ -0,0 +1,19 @@
+//Poder espcial- Dano de 35 Perder 15
+import java.util.*;
+
+class Guerreiro extends Personagem{
+  public Guerreiro(String nome, int tribo, int classe){
+    super(nome, tribo, classe);
+  } 
+  
+  public void poderEspecial(int jogador, Vector<Personagem> personagem_partida){
+
+    Scanner teclado = new Scanner(System.in);
+
+    System.out.printf("\n Digite seu oponente: ");
+    int openente1 = teclado.nextInt()-1;
+
+    personagem_partida.get(openente1).setVida(personagem_partida.get(openente1).getVida()-35);
+    personagem_partida.get(jogador).setVida(personagem_partida.get(jogador).getVida()-15);
+  }
+}
