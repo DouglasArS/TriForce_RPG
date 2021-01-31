f2a893f25b329af5eb1abd776ff08faa29f6b8b6
CarlosEstellita <74671164+CarlosEstellita@users.noreply.github.com>
2021-01-31 19:14:10 -0300

    Update FuncionalidadesBatalha.java

--- TriForce_RPG/src/Game/FuncionalidadesBatalha.java
+++ TriForce_RPG/src/Game/FuncionalidadesBatalha.java
@@ -1,18 +1,3 @@
-<<<<<<< HEAD:TriForce_RPG/src/Game/FuncionalidadesBatalha.java
-public interface FuncionalidadesBatalha
-{
-  public void atacar(int atacante, int oponente);
-  public void defender(int jogador);
-  public void powerUp(int jogador);
-  public void mostrarPersonagens();
-  public int personagemVivos();
-  public void luta();
-  public boolean criarPersonagem();
-  public void getNomesArquivos();
-  public void mostrarArquivos();
-  public boolean carregarPersonagem();
-}
-=======
 public interface FuncionalidadesBatalha
 {
   public void atacar(int atacante, int oponente);
@@ -28,4 +13,3 @@ public interface FuncionalidadesBatalha
   public boolean excluirPersonagem();
   public boolean editarPersonagem();
 }
->>>>>>> 36a30aeb154158d2f0c971f932ab1c488f0f72aa:Game_RPG/src/game_rpg/FuncionalidadesBatalha.java
